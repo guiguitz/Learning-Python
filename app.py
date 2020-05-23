fc8574b558f8eb2f98d21345870a7a67f6bc350a
Guilherme Vinícius Amorim <guilherme.vini65@gmail.com>
2020-05-23 00:27:37 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+name = input("Enter your name:")
+print("hello " + name + "!")
+
+# NUMBERS:
+# from math import *
+# my_number = 5
+# print(ceil(3.7))
+# print(str(my_number) + " is my favorite number")
+# my_number = -5
+# print(str(abs(my_number)) + " is my favorite number")
+# print(round(3.4))
+
+# Variables types:
+# character_name = "Jown"           #string variable
+# character_age = 70                #number variable
+# character_is_male = True          #boolean variable
+#
+# String Functions:
+# phrase = "Gui Lindo"
+# print(phrase.replace("Lindo", "Gostoso"))
+# print(phrase.index(" Lind"))
+# print(len(phrase))
+# print(phrase[2])
+# print(phrase.upper().isupper())
+# print(phrase.upper())
+# print(phrase.lower())
+# print(phrase.islower())
+# print(character_name + " is " + character_age + " years old\n")
+# print(character_age)
+# print("The character name is "+character_name+", ")
+# print("The character age is "+character_age+",\n")
+# print("The character name and age are "+character_name+" and "+character_age+"")
+
